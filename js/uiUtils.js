089ba794b9213cc787d9e3b1fff8f5011c862b3a
Heather <heather@travelswithchesky.com>
2017-10-18 13:44:28 -0400

    testing change
    in github desktop

--- js/uiUtils.js
+++ js/uiUtils.js
@@ -13,6 +13,7 @@
  | See the License for the specific language governing permissions and
  | limitations under the License.
  */
+ //making a change to test github desktop
 define([
 	"dijit/registry",
 	"dojo/_base/array",
